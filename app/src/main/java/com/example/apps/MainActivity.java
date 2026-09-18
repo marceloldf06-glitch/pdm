5fd5cc7075c93c0e9a214ef470dffe7d2f082475
Marcelo <marcelo.ldf06@aluno.ifsc.edu.br>
2026-09-18 11:58:09 -0300

    new imgs

--- app/src/main/java/com/example/apps/MainActivity.java
+++ app/src/main/java/com/example/apps/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.apps;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,49 +12,41 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.Random;
 
-import kotlin.random.URandomKt;
-
 public class MainActivity extends AppCompatActivity {
-    Random rng = new Random();;
-    double contador = 0;
-    double sum = 1;
 
-
+    int posicao = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Button b = findViewById(R.id.button);
-        TextView tv = findViewById(R.id.textView);
-
-        EditText edmin = findViewById(R.id.edmin);
-        EditText edmax = findViewById(R.id.edmax);
-
-        b.setOnClickListener(view -> {
-            int min = Integer.parseInt(edmin.getText().toString());
-            int max = Integer.parseInt(edmax.getText().toString());
-            contador = contador + Math.round(sum);
-            tv.setText(Double.toString(contador));
-            tv.setBackgroundColor(rng.nextInt(min, max));
-            tv.setTextSize(55);
-
-            if (contador >= 20*sum){
-                contador = contador - 20*sum;
-                sum = sum * 1.05;
-            }
-
-
-
-        });
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Integer[] imgs = new Integer[]{
+                R.drawable.cachorro,
+                R.drawable.gardem,
+                R.drawable.happy,
+                R.drawable.patinho,
+                R.drawable.porquinho
+        };
+        ImageView iv = findViewById(R.id.imageView2);
+
+        Button btnvol, btnpro;
+        btnvol = findViewById(R.id.btnvolt);
+        btnpro = findViewById(R.id.btnprox);
+
+        btnpro.setOnClickListener(view -> {
+            posicao++;
+            iv.setImageResource(imgs[posicao]);
+            if (posicao >= imgs.length){
+                posicao = 0;
+            }
+        });
+
     }
 }
